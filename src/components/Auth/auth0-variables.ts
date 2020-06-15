1be309141065051bf3cdcea3692920a4419cb013
hardy1618 <hardhikdreddy@gmail.com>
2020-06-16 01:20:16 +0530

    added callback url

--- src/components/Auth/auth0-variables.ts
+++ src/components/Auth/auth0-variables.ts
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'dev-k36jboch.auth0.com',
   clientId: 'RLXRIQ077O10Ugwli3Ft89JWXabIBmyx',
-  callbackUrl: 'http://localhost:3000'
+  callbackUrl: 'https://ancient-falls-35391.herokuapp.com/'
 };
