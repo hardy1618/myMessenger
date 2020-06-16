ae37785eeb0951adc7c46deb951c694c64049947
hardy1618 <hardhikdreddy@gmail.com>
2020-06-16 10:47:04 +0530

    corrected left and right

--- src/components/MessagesList/Message.tsx
+++ src/components/MessagesList/Message.tsx
@@ -8,9 +8,9 @@ interface props {
 
 const Message: React.FC<props> = (props) => (
     <div className={"Message" + props.pos}>
-        {props.pos === 'left' &&
-            (<div className="Message-username">me</div>)}
         {props.pos === 'right' &&
+            (<div className="Message-username">me</div>)}
+        {props.pos === 'left' &&
             (<div className="Message-username">{props.sender_name}</div>)}
         {/* <div className="Message-box"> */}
             <div className="Message-text">{props.content}</div>
